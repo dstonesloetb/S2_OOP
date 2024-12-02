496aac1a5417b1c71bb6d5f07b87566bdd180701
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-12-02 16:16:51 +0000

    Create Harkonnen.java

---
+++ Harkonnen.java
@@ -0,0 +1,6 @@
+// Subclass representing Harkonnens
+class Harkonnen extends DuneCharacter {
+    public Harkonnen(String name, int health) {
+        super(name, "Harkonnen", "Noble", health);
+    }
+}
