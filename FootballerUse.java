8875200257b77669622a67ee9294cf2019498631
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-03-19 09:49:27 +0000

    Create FootballerUse.java

---
+++ FootballerUse.java
@@ -0,0 +1,13 @@
+ // FootballerUse.java
+
+public class FootballerUse {
+  public static void main(String[] args) {
+
+    //See lab 11 and 12 for more details 
+    
+    System.out.println("Hello world!");
+
+    
+  } //end main 
+
+} //end class 
