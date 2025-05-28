2357d9f32c8862d4c6a9f96fa87eea4eb607b2ec
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-05-28 13:51:08 +0000

    Pending changes exported from your codespace

--- DuneCharacter.java
+++ DuneCharacter.java
@@ -110,21 +110,4 @@ class BeneGesserit extends DuneCharacter {
     }
 }
 
-// Example of usage - DuneCharactersDemo
-public class Main {
-  public static void main(String[] args) {
 
-    //Create a Fremen object
-    Fremen paul = new Fremen("Paul Atreides", "Sietch Tabr");
-
-    //Create a BeneGesserit object   
-    BeneGesserit jessica = new BeneGesserit("Lady Jessica", "Reverend Mother");
-
-    //Display both objects
-    System.out.println(paul.toString());
-
-    System.out.println("\n");
-    
-    System.out.println(jessica);
-  }
-}
--- UsingCarClass.java
+++ UsingCarClass.java
@@ -1,5 +1,3 @@
-package com.mycompany.oopexamples;
-
 import java.util.Scanner;
 
 /**
